92fbb23bc087c39a26678663ee7edd19f04ba3c1
Jameson Hill <stevenjamesonhill@gmail.com>
2017-04-20 17:37:47 -0400

    server changes

--- server.js
+++ server.js
@@ -12,3 +12,37 @@ app.use('/blog-post', blogRouter);
 app.listen(process.env.PORT || 8080, () => {
   console.log(`your app is listenting on port ${process.env.PORT || 8080}`);
 });
+
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
