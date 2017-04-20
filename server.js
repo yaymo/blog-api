ed2e60723424cb4dc7577fe09238855bb04aec7b
Jameson Hill <stevenjamesonhill@gmail.com>
2017-04-20 17:43:43 -0400

    typo fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const blogPostsRouter = require('./blogPostsRouter');
+const blogRouter = require('./blogRouter');
 const app = express();
 
 
